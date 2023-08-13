a16cc10093a487b6365d39043e367ed3b3b6f7d2
Nils Osswald <imrebux@gmail.com>
2023-08-13 22:08:18 +0200

    :construction: Basic stack page

---
+++ src/routes/stack.jsx
@@ -0,0 +1,24 @@
+import { For } from 'solid-js';
+import stack from '~/stack.json';
+
+const Stack = () => {
+  return (
+    <main class='p-5 text-black dark:text-white'>
+      <p class='mb-8 text-3xl font-semibold'>Technologies I've been working with recently</p>
+      <div class='flex flex-col gap-4'>
+        <For each={Object.entries(stack)}>
+          {([group, entries]) => (
+            <section>
+              <p class='text-lg font-semibold capitalize'>{group}</p>
+              <ul>
+                <For each={entries}>{(entry) => <li>{entry}</li>}</For>
+              </ul>
+            </section>
+          )}
+        </For>
+      </div>
+    </main>
+  );
+};
+
+export default Stack;
