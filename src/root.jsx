df6a5078d0b0bf246066dbc9be7de76408b61af4
Nils Osswald <imrebux@gmail.com>
2023-08-13 12:56:25 +0200

    :mag: Add more meta tags

--- src/root.jsx
+++ src/root.jsx
@@ -19,18 +19,28 @@ const Root = () => {
   return (
     <Html lang='en' dir='ltr'>
       <Head>
-        <Title>Nils Osswald</Title>
-        <Link rel='icon' href='/favicon.ico' />
         <Meta charset='utf-8' />
         <Meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta name='theme-color' content='#4287f5' />
+        <Title>Nils Osswald</Title>
+        <Link rel='icon' href='/favicon.ico' />
 
-        {/* Meta tags */}
+        {/* Primary tags */}
+        <Meta name='title' content='Nils Osswald' />
+        <Meta name='description' content='Personal portfolio of Nils Osswald' />
+
+        {/* Open Graph tags */}
         <Meta property='og:title' content='Nils Osswald' />
         <Meta property='og:type' content='website' />
-        <Meta property='og:url' content='https://nilsosswald.de' />
+        <Meta property='og:url' content='https://www.nilsosswald.de/' />
         <Meta property='og:image' content='https://www.nilsosswald.de/favicon.ico' />
         <Meta property='og:description' content='Personal portfolio of Nils Osswald' />
+
+        {/* Twitter tags */}
+        <Meta property='twitter:url' content='https://www.nilsosswald.de/' />
+        <Meta property='twitter:title' content='Nils Osswald' />
+        <Meta property='twitter:description' content='Personal portfolio of Nils Osswald' />
+        <Meta property='twitter:image' content='https://www.nilsosswald.de/favicon.ico' />
       </Head>
       <Body class='flex h-screen flex-col bg-background-light dark:bg-background-dark'>
         <ErrorBoundary>
