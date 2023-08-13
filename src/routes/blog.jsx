0d569e1332f78251c9baec9ee4982d91ed7f83c7
Nils Osswald <imrebux@gmail.com>
2023-08-13 23:41:41 +0200

    :construction: Placeholder blog page

---
+++ src/routes/blog.jsx
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <div class='flex h-full flex-col items-center justify-center text-black dark:text-white'>
+      <p class='text-[2rem] font-semibold'>🚧 Work In Progress</p>
+    </div>
+  );
+};
+
+export default Blog;
