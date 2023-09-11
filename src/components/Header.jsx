0cb1e68f72d2c6773fe4c1335edfb968a5dc87d7
Nils Osswald <imrebux@gmail.com>
2023-09-11 19:42:57 +0200

    :lipstick: Update landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
     <header class='flex select-none items-center justify-between border-b bg-surface-light p-4 dark:bg-surface-dark sm:border-0 sm:bg-transparent sm:py-8 sm:dark:bg-transparent'>
       {/* Spacer */}
       <div class='hidden sm:block'>
+        {/* TODO: Replace this with an image */}
         <div class='flex h-10 w-10 items-center justify-center rounded-full border bg-surface-light text-zinc-600 dark:bg-surface-dark dark:text-zinc-400'>
           N
         </div>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,66 @@
+import { A } from 'solid-start';
+import socials from '~/data/socials.json';
+
 const About = () => {
   return (
-    <div class='text-4xl font-bold leading-snug text-black dark:text-white'>
-      <p>Hey There! 👋</p>
-      <p>
-        I'm <span class='text-primary'>Nils</span>, A Full Stack
-      </p>
-      <p>Software Engineer</p>
+    <div class='mt-8 flex flex-col gap-10 sm:mt-0'>
+      <div class='flex flex-col-reverse justify-between gap-10 sm:flex-row'>
+        {/* Left */}
+        <div class='flex flex-col gap-10 text-center sm:text-left'>
+          {/* Title */}
+          <div class='text-4xl font-bold leading-snug text-black dark:text-white'>
+            <p>Hey There! 👋</p>
+            <p>
+              I'm <span class='text-primary'>Nils</span>, A Full Stack
+            </p>
+            <p>Software Engineer</p>
+          </div>
+
+          {/* Introduction */}
+          <div class='max-w-[60ch] text-black dark:text-white'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim rem, laudantium
+              nostrum aut ea nihil deserunt omnis corporis vitae obcaecati quidem a soluta, sed
+              adipisci, atque cupiditate? Cupiditate, vel?
+            </p>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div class='flex w-full justify-center sm:w-fit'>
+          {/* TODO: Replace this with an image */}
+          {/* TODO: Add fancy animation */}
+          <div class='h-40 w-40 rounded-full border-2 bg-surface-light dark:bg-surface-dark' />
+        </div>
+      </div>
+      <div class='flex w-full justify-center gap-4 sm:justify-normal'>
+        <a
+          href={socials.email}
+          class='min-w-[8rem] select-none rounded-lg bg-primary p-2 text-center text-black transition-all hover:scale-105 hover:shadow-lg hover:shadow-primary/50 dark:text-white'
+        >
+          Contact Me
+        </a>
+        <A
+          href='/projects'
+          class='min-w-[8rem] select-none rounded-lg border border-primary p-2 text-center text-black transition-all hover:scale-105 hover:shadow-lg hover:shadow-primary/50 dark:text-white'
+        >
+          See My Work
+        </A>
+      </div>
+      <hr />
+      <div class='flex flex-col items-center gap-2 sm:items-start'>
+        <img
+          src='https://skillicons.dev/icons?i=kotlin,java,js,ts,react,css,tailwind'
+          alt='Stack'
+        />
+        <p class='text-zinc-600 dark:text-zinc-400'>
+          View more in my{' '}
+          <A class='text-primary' href='/stack'>
+            tech stack
+          </A>
+        </p>
+      </div>
+      <hr />
     </div>
   );
 };
