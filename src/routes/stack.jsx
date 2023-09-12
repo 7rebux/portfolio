356186db25b8b5f7ae88eca5a0208c7f8f746d3d
Nils Osswald <imrebux@gmail.com>
2023-09-12 14:27:26 +0200

    :construction: Update stack page

--- src/routes/stack.jsx
+++ src/routes/stack.jsx
@@ -3,16 +3,27 @@ import stack from '~/data/stack.json';
 
 const Stack = () => {
   return (
-    <div class='text-black dark:text-white'>
-      <p class='mb-8 text-3xl font-semibold'>Technologies I've been working with recently</p>
-      <div class='flex flex-col gap-4'>
+    <div class='flex flex-col gap-4'>
+      <p class='text-3xl font-semibold text-black dark:text-white'>My Tech Stack</p>
+      <div class='flex flex-col gap-3'>
         <For each={Object.entries(stack)}>
           {([group, entries]) => (
-            <section>
-              <p class='text-lg font-semibold capitalize'>{group}</p>
-              <ul>
-                <For each={entries}>{(entry) => <li>{entry}</li>}</For>
+            <section class='flex flex-col gap-2'>
+              <p class='text-lg font-semibold text-black dark:text-white'>{group}</p>
+              <ul class='flex flex-col gap-2'>
+                <For each={entries}>
+                  {({ title, description, icon }) => (
+                    <li class='flex items-start gap-2'>
+                      <img src={icon} />
+                      <div>
+                        <p class='text-black dark:text-white'>{title}</p>
+                        <p class='text-zinc-700 dark:text-zinc-300'>{description}</p>
+                      </div>
+                    </li>
+                  )}
+                </For>
               </ul>
+              <hr />
             </section>
           )}
         </For>
