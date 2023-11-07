9183ccfa59381df7ab49f0c9605dae4cdf702ddc
Nils Osswald <contact@nilsosswald.de>
2023-11-07 18:38:18 +0100

    :heavy_plus_sign: Add @vercel/analytics

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,8 +1,11 @@
 import { mount, StartClient } from 'solid-start/entry-client';
 import { themeCookie, setThemeInner } from '~/utils/theme.js';
+import { inject } from '@vercel/analytics';
 
 mount(() => {
   themeCookie.parse(document.cookie).then((theme) => {
+    inject();
+
     setThemeInner({
       theme: theme ?? 'system',
       systemTheme: window.matchMedia('(prefers-color-scheme: dark)').matches,
