acc6f6e6ec15a578da707722d62536d8ae150e9d
Nils Osswald <contact@nilsosswald.de>
2023-12-02 11:14:48 +0100

    :lipstick: Update blog post header

--- src/components/PostHeader.jsx
+++ src/components/PostHeader.jsx
@@ -1,21 +1,14 @@
-import { CalenderIcon, PersonIcon } from '~/components/icons.jsx';
+import { CalenderIcon } from '~/components/icons.jsx';
 
-export function PostHeader({ title, description, date, author }) {
+export function PostHeader({ title, description, date }) {
   return (
-    <div class='mt-6 flex flex-col items-center sm:mt-0 sm:items-start'>
-      <h1 class='m-0 mb-4'>{title}</h1>
-      <h4 class='m-0'>{description}</h4>
-      <div class='inline-flex gap-6'>
-        <div class='inline-flex items-center gap-2'>
-          <CalenderIcon />
-          <p>{date}</p>
-        </div>
-        <p>•</p>
-        <div class='inline-flex items-center gap-2'>
-          <PersonIcon />
-          <p>{author}</p>
-        </div>
+    <div class='mt-6 flex flex-col gap-4 sm:mt-0'>
+      <div class='inline-flex items-center gap-2'>
+        <CalenderIcon />
+        <p class='m-0 text-sm'>{date}</p>
       </div>
+      <h1 class='m-0'>{title}</h1>
+      <h4 class='m-0'>{description}</h4>
       <hr class='my-2 w-full' />
     </div>
   );
